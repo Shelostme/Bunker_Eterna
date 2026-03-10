ce0422613aaf2511b326ebf9234092572dccc251
Shelostme <dmitrishome@gmail.com>
2026-03-10 11:45:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import sys
 import os
 
 # ----- SOLUCIÓN PERMANENTE: Asegurar que Python encuentre los módulos -----
-# Obtenemos el directorio donde está este script usando os.path (más robusto)
 current_dir = os.path.dirname(os.path.abspath(_file_))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
