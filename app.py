0eb4b3adbf160123fb40bce699446b2ea202027f
Shelostme <dmitrishome@gmail.com>
2026-03-16 07:33:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from eterna_core import (
 )
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 
 if os.path.exists(".env"):
     from dotenv import load_dotenv
